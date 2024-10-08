0718eb0209ba63a6a31f4d9addb8f431ef855c5d
Mark Brian <markbkiunga2017@gmail.com>
2024-10-08 12:47:04 +0300

    Completes Codealong

---
+++ app/migrations/versions/e6523df9a586_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: e6523df9a586
+Revises: 
+Create Date: 2024-10-08 12:35:31.768916
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e6523df9a586'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
